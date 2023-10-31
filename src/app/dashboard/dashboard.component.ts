78010e6eeb16ebb80d9592fd4dcbfaf73ac462f8
Qharny <cannycc1020@gmail.com>
2023-10-31 11:54:54 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   src/app/app.module.ts
    	new file:   src/app/dashboard/dashboard.component.css
    	new file:   src/app/dashboard/dashboard.component.html
    	new file:   src/app/dashboard/dashboard.component.spec.ts
    	new file:   src/app/dashboard/dashboard.component.ts

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent {
+  // any logic for the dashboard goes here
+}
