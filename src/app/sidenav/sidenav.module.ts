60d5dc88747e810fb2744d543be6ac525f09071f
Qharny <cannycc1020@gmail.com>
2023-10-31 14:50:50 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is ahead of 'origin/main' by 1 commit.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/app/app.component.css
    	new file:   src/app/dashboard/dashboard.module.ts
    	new file:   src/app/header/header.module.ts
    	new file:   src/app/home/home.module.ts
    	modified:   src/app/sidenav/sidenav.component.html
    	modified:   src/app/sidenav/sidenav.component.ts
    	new file:   src/app/sidenav/sidenav.module.ts
    	modified:   src/styles.css

---
+++ src/app/sidenav/sidenav.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon'; // Import MatIconModule
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatListModule,
+    MatIconModule
+  ]
+})
+export class SidenavModule { }
