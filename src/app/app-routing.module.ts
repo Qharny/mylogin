37274ead9dac8f48006109684f7988886ff85899
Qharny <cannycc1020@gmail.com>
2023-10-31 13:18:57 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   src/app/app-routing.module.ts
    	modified:   src/app/app.component.css
    	modified:   src/app/app.component.html
    	modified:   src/app/dashboard/dashboard.component.html
    	modified:   src/app/header/header.component.html
    	modified:   src/app/sidenav/sidenav.component.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { HeaderComponent } from './header/header.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
   {path: "", redirectTo: "login", pathMatch: "full"},
-  {path: "**", redirectTo: "login", pathMatch: "full"}
+  {path: "**", redirectTo: "login", pathMatch: "full"},
+  { path: 'sidenav', component: SidenavComponent },
+  { path: 'header', component: HeaderComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
 ];
 
 @NgModule({
