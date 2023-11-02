a857610a38d70f39d0ed4a04d370fdd99a61a4fb
Qharny <cannycc1020@gmail.com>
2023-11-02 10:18:17 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   src/app/app.component.css
    	modified:   src/app/app.component.html
    	modified:   src/app/home/home.component.html
    	modified:   src/app/home/home.component.ts
    	modified:   src/app/sidenav/sidenav.component.css
    	modified:   src/app/sidenav/sidenav.component.html

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  transactions = [
+    { transactionId: 1, Name: 'John', amount: 1000, transactionDate: '2018-05-01' },
+    { transactionId: 2, Name: 'John', amount: 2000, transactionDate: '2018-05-02' },
+    { transactionId: 3, Name: 'John', amount: 3000, transactionDate: '2018-05-03' },
+    { transactionId: 4, Name: 'John', amount: 4000, transactionDate: '2018-05-04' },
+    { transactionId: 5, Name: 'John', amount: 5000, transactionDate: '2018-05-05' },
+    { transactionId: 6, Name: 'John', amount: 6000, transactionDate: '2018-05-06' },
+    { transactionId: 7, Name: 'John', amount: 7000, transactionDate: '2018-05-07' },
+    { transactionId: 8, Name: 'John', amount: 8000, transactionDate: '2018-05-08' },
+    { transactionId: 9, Name: 'John', amount: 9000, transactionDate: '2018-05-09' },
+    { transactionId: 10,Name: 'John', amount: 10000, transactionDate: '2018-05-09' }
+  ];
 
 }
